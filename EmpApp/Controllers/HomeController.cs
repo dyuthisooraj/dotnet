fb401b965472dfd4d986a9099c81d0b07f879b90
dyuthisooraj <dyuthiminnu99@gmail.com>
2022-10-28 18:56:59 +0530

    first commit

---
+++ EmpApp/Controllers/HomeController.cs
@@ -0,0 +1,162 @@
+﻿using EmpApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace EmpApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public async Task<IActionResult> EmployeeView()
+        {
+            HttpClientHandler clienthandler = new HttpClientHandler();
+            clienthandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslpolicyerrors) => { return true; };
+
+            HttpClient client = new HttpClient(clienthandler);
+            client.BaseAddress = new Uri("https://localhost:7170");
+            List<EmployeeModelMVC>? emplist = new List<EmployeeModelMVC>();
+
+            HttpResponseMessage res = client.GetAsync("api/Employee/Get").Result;
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                emplist = JsonConvert.DeserializeObject<List<EmployeeModelMVC>>(result);
+            }
+            return View(emplist);
+
+        }
+        public async Task<ActionResult> Post()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7170");
+            List<DesignationMVC>? designationTemp = new List<DesignationMVC>();
+
+            HttpResponseMessage res = await client.GetAsync("api/Designation");
+
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                designationTemp = JsonConvert.DeserializeObject<List<DesignationMVC>>(result);
+                ViewData["designationtemp"] = designationTemp;
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(EmployeeModelMVC empreg)
+        {
+
+            HttpClientHandler clienthandler = new HttpClientHandler();
+            clienthandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslpolicyerrors) => { return true; };
+
+
+            HttpClient client = new HttpClient(clienthandler);
+            client.BaseAddress = new Uri("https://localhost:7170");
+            var postTask = client.PostAsJsonAsync<EmployeeModelMVC>("api/Employee/Post/", empreg);
+            postTask.Wait();
+            var Result = postTask.Result;
+            if (Result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("EmployeeView");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(string UserName)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7170");
+            await client.DeleteAsync($"api/Employee/Delete/{UserName}");
+            return RedirectToAction("EmployeeView");
+
+        }
+        //public async Task<IActionResult> Edit(string username)
+        //{
+
+        //    var client = new HttpClient();
+        //    client.BaseAddress = new Uri("https://localhost:7170");
+        //    TempData temp = new TempData();
+        //    HttpResponseMessage res = await client.GetAsync($"api/Employee/Get/{username}");//Api layr
+        //    if (res.IsSuccessStatusCode)
+        //    {
+        //        var result = res.Content.ReadAsStringAsync().Result;
+
+
+        //        temp = JsonConvert.DeserializeObject<TempData>(result);
+        //    }
+
+
+        //    return View(temp);
+        //}
+        [HttpPost]
+        public async Task<IActionResult> Edit(TempData temp)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7170");
+            var postTask = client.PostAsJsonAsync<TempData>("api/Employee/Edit", temp);
+            postTask.Wait();
+            var Result = postTask.Result;
+            if (Result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("EmployeeView");
+            }
+            return View();
+        }
+
+
+        public async Task<IActionResult> Edit(string username)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7170");
+            List<DesignationMVC>? designationTemp = new List<DesignationMVC>();
+
+            HttpResponseMessage des = await client.GetAsync("api/Designation");
+
+            if (des.IsSuccessStatusCode)
+            {
+                var result = des.Content.ReadAsStringAsync().Result;
+                designationTemp = JsonConvert.DeserializeObject<List<DesignationMVC>>(result);
+                ViewData["designationtemp"] = designationTemp;
+            }
+
+            TempData employee = new TempData();
+            HttpResponseMessage res = await client.GetAsync($"api/Employee/Get/{username}");
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                employee = JsonConvert.DeserializeObject<TempData>(result);
+            }
+
+
+            return View(employee);
+        }
+
+        public ActionResult designation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> designation(DesignationMVC designationClass)
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7170");
+            var postTask = client.PostAsJsonAsync<DesignationMVC>("api/Designation/designation", designationClass);
+
+            /*  var postTask = client.PostAsJsonAsync<DesignationClass>("api/Designation/Designation", designationClass)*/
+            postTask.Wait();
+            var Result = postTask.Result;
+            if (Result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("DashBoard","AdminMVC");
+            }
+            return View();
+        }
+        
+
+
+    }
+}
+
---
+++ EmployeeAppApiLayer/Controllers/EmployeeController.cs
@@ -0,0 +1,65 @@
+﻿using EmployeeAppModels;
+using EmployeeAppBusiness.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EmployeeAppApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployee _emp;
+        public EmployeeController(IEmployee emp)
+        {
+            _emp = emp;
+        }
+
+        [HttpGet("Get")]
+        
+        public ActionResult<List<EmployeeRegistration>> Get()
+        {
+            var result = _emp.Get();
+            return Ok(result);
+        }
+
+        [HttpPost("Post")]
+        //[Route]
+        public IActionResult Post(EmployeeRegistration emp1)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Is not valid");
+            _emp.Post(emp1);
+            return Ok();
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete]
+        [Route("Delete/{UserName}")]
+        public ActionResult Delete(string username)
+        {
+            _emp.Delete(username);
+            return Ok();
+
+        }
+
+        [HttpGet]
+        [Route("Get/{username}")]
+        public ActionResult<EmployeeRegistration> Get(string UserName)
+        {
+            return _emp.Get(UserName);
+         }
+
+        [HttpPost]
+        [Route("Edit")]
+        public IActionResult Edit(EmployeeRegistration emp1)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Is not valid");
+            _emp.Edit(emp1);
+            return Ok();
+        }
+
+    }
+
+}
